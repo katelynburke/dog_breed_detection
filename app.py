e275b3d930c2936e833e799e1cfc1f05ef0fe19f
kglibrarian <karen.gutzman@northwestern.edu>
2019-07-24 20:22:19 -0500

    commenting out database

--- app.py
+++ app.py
@@ -22,20 +22,20 @@ from flask_sqlalchemy import SQLAlchemy
 ##http://flask.pocoo.org/docs/1.0/tutorial/database/
 
 ## create a connection with the database
-engine = create_engine("sqlite:///static/db/weatherData.sqlite", connect_args={'check_same_thread': False})
+#engine = create_engine("sqlite:///static/db/weatherData.sqlite", connect_args={'check_same_thread': False})
 ## reflect an existing database into a new model
-Base = automap_base()
+#Base = automap_base()
 ## reflect the tables
-Base.prepare(engine, reflect=True)
+#Base.prepare(engine, reflect=True)
 # reflect all of the classes mapped to the Base
-Base.classes.keys()
+#Base.classes.keys()
 # create a "Metadata" Layer That Abstracts our SQL Database
-Base.metadata.create_all(engine)
+#Base.metadata.create_all(engine)
 # Save a reference to the each database table like this:  ConsumptionSector = Base.classes.energy_consumption_sector
 ## ADD CODE HERE!!
 
 # Create our session (link) from Python to the DB
-session = Session(engine)
+#session = Session(engine)
 
 #################################################
 # Flask Setup
