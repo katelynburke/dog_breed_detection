21e0a8c2f3a6d9e19cd6ed5c580bbdf56fdb0726
kglibrarian <karen.gutzman@northwestern.edu>
2019-07-29 18:34:51 -0500

    commented out some stuff kg

--- app.py
+++ app.py
@@ -1,62 +1,42 @@
 # import necessary libraries
-import sqlite3
+#import sqlite3
 import json
-import numpy as np
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-from flask import (
-    Flask,
-    render_template,
-    url_for,
-    jsonify,
-    request,
-    redirect)
+#import numpy as np
+# import sqlalchemy
+# from sqlalchemy.ext.automap import automap_base
+# from sqlalchemy.orm import Session
+# from sqlalchemy import create_engine, func
+# from flask import (
+#     Flask,
+#     render_template,
+#     url_for,
+#     jsonify,
+#     request,
+#     redirect)
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 from flask import * 
 import os
 import module23
 import training_models
 from flask import Flask,abort,render_template,request,redirect,url_for
 from werkzeug import secure_filename
-#################################################
-## Database Connection using SQLAlchemy
-#################################################
-##http://flask.pocoo.org/docs/1.0/tutorial/database/
-
-## create a connection with the database
-#engine = create_engine("sqlite:///static/db/weatherData.sqlite", connect_args={'check_same_thread': False})
-## reflect an existing database into a new model
-#Base = automap_base()
-## reflect the tables
-#Base.prepare(engine, reflect=True)
-# reflect all of the classes mapped to the Base
-#Base.classes.keys()
-# create a "Metadata" Layer That Abstracts our SQL Database
-#Base.metadata.create_all(engine)
-# Save a reference to the each database table like this:  ConsumptionSector = Base.classes.energy_consumption_sector
-## ADD CODE HERE!!
-
-# Create our session (link) from Python to the DB
-#session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
 CORS(app)
-UPLOAD_FOLDER = './static/images'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
+
 #################################################
 # Flask Routes
 #################################################
 
+UPLOAD_FOLDER = './static/images'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
+
 # create route that renders index.html template
 @app.route("/")
-# def home():
-#     return render_template("index.html")
 def upload():  
     return render_template("index.html")  
  
@@ -68,9 +48,8 @@ def success():
         if file:
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-            #test = module23.add(filename) 
             train = training_models.trainer(filename)
-        return render_template('success.html', filename=filename, test=test, train=train)    
+        return render_template('success.html', filename=filename, train=train)    
 # create route that renders about.html template
 @app.route("/about")
 def About():
